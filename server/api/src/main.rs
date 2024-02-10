cccc810a29e8beeb487bb6235493fbccfe5d6ad5
ningenMe <amohkuru@gmail.com>
2024-02-11 02:06:45 +0900

    [add] cors

--- server/api/src/main.rs
+++ server/api/src/main.rs
@@ -1,9 +1,23 @@
+use axum::http::{HeaderValue, Method};
+use tower_http::cors::CorsLayer;
+
 mod controller;
 
 #[tokio::main]
 async fn main() {
     let api = controller::bookmark_controller::Api{};
-    let router = generated::server::new(api);
+    let layer = CorsLayer::new()
+    .allow_origin([
+        "https://ningenme.net".parse::<HeaderValue>().unwrap(),
+        "http://localhost:3000".parse::<HeaderValue>().unwrap()
+    ])
+    .allow_methods([
+        Method::GET, 
+        Method::POST,
+        Method::OPTIONS
+    ])
+    ;
+    let router = generated::server::new(api).layer(layer);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:50051")
         .await
         .unwrap();
